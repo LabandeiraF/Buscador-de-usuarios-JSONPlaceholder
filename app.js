ef979356c3bf0de0bed939fcf47d91597604d0c8
Fernando Labandeira <nandolabandeira15@gmail.com>
2023-11-29 15:12:34 -0300

    Arreglo problema con el fetch a la api, en funcionamiento

--- app.js
+++ app.js
@@ -1,49 +1,46 @@
 
 const buscador = document.getElementById('input-buscador');
 const btnBuscador = document.getElementById('btn-buscador');
-const usuario = document.getElementById('usuario');
+const contenedorResultado = document.getElementById('usuario');
 
 btnBuscador.addEventListener('click', async function () {
     const nombreUsuario = buscador.value.trim();
 
     if (nombreUsuario === '') {
-        alert('ingrese un nombre de usuario valido');
+        alert('Ingrese un nombre de usuario válido');
         return;
     }
 
     try {
-        const response = await fetch(`https://jsonplaceholder.org/users?name=${nombreUsuario}`);
+        const response = await fetch(`https://jsonplaceholder.typicode.com/users?name=${nombreUsuario}`);
         if (!response.ok) {
-            console.error('informacion no encontrada');
+            console.error('Información no encontrada');
         }
         const usuarios = await response.json();
-        Resultado(usuarios);
+        Resultado(usuarios, contenedorResultado); 
     } catch (error) {
         console.error(error);
         alert('Ocurrió un error');
     }
 });
 
-function Resultado(usuarios) {
-    usuario.innerHTML = '';
+function Resultado(usuarios, contenedor) {
+    contenedor.innerHTML = ''; 
 
     if (usuarios.length === 0) {
-        usuario.innerHTML = '<p>usuario no existe en la base de datos</p>';
+        contenedor.innerHTML = '<p>Usuario no existe en la base de datos</p>';
         return;
     }
 
     usuarios.forEach(function (usuario) {
         const tarjeta = document.createElement('div');
-        tarjeta.classList.add('card', 'mb-3');
+        tarjeta.classList.add('card');
         tarjeta.innerHTML = `
             <div class="card">
                 <p>${usuario.name}</p>
                 <p>Email: ${usuario.email}</p>
                 <p>Dirección: ${usuario.address.city}, ${usuario.address.street}</p>
                 <p>Teléfono: ${usuario.phone}</p>
-            </div>
-        `;
-        usuario.appendChild(tarjeta);
-    });
-}
-
+            </div>`;
+        contenedor.appendChild(tarjeta); 
+    });}
